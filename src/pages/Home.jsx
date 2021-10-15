bcd32e09550d3f72f99454552308e77c2bcc229a
EmmaHammar <emma.s.hammar@gmail.com>
2021-10-15 14:02:18 +0200

    adding #6 printing members + catching clicked id - saved in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,22 @@ import ProjectList from '../components/ProjectList';
 import useFetch from 'use-http';
 import { useState, useEffect } from 'react';
 
-
 export default function Home() {
 
     const { get} = useFetch("http://localhost:3000");
     // const { get, post, del, patch } = useFetch("http://localhost:3000");
     const [projects, setProjects] = useState([]);
 
+    const [members, setMembers] = useState([]);
+    const [clickedId, setClickedId] = useState(Number);
+
     useEffect(() => {
         getProjects();
-    }, [])
 
+        let members = ["Axel", "Emma", "Sandra", "Sara K", "Sara M"];
+        setMembers(members);
+    }, [])
+    
     const getProjects = () => {
         get("/projects")
             .then(data => {
@@ -23,10 +28,19 @@ export default function Home() {
             })
     };
 
+    const handleClick = (evt) => {
+        setClickedId(evt.target.id);
+    }
+
     return (
         <>
             <Header />
             <ProjectList projects={projects}/>
+
+            {members.map( (member, i) => (
+                <button onClick={handleClick} id={++i} key={i}>{member}</button>
+            )) }
+            
             <Footer />
         </>
     )
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -7,6 +7,8 @@ export default function Project({projects}) {
 
     console.log("prjects", projects)
 
+
+
     return (
 
         <>
