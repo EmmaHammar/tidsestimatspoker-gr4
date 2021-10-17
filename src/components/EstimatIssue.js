ff558e82750c8d3272be5b5b7baae503499d1406
Sara Sedaghat <75505464+Sarasedaghat8960@users.noreply.github.com>
2021-10-17 22:25:42 +0200

    make table and succed to ass one estimate issue time to state

--- src/components/EstimatIssue.js
+++ src/components/EstimatIssue.js
@@ -1,96 +1,117 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import styles from '../styles/Estimate.module.scss'
 import { uuid } from 'uuidv4';
 import useFetch from 'use-http';
 import { useState} from 'react';
+
+
 export default function EstimatIssue({projects,setProjects}) {
-        const { patch} = useFetch("http://localhost:3000");
-       
+   
+        const { get,post} = useFetch("http://localhost:3000");
+        const [oneProject,setOneProject]=useState([])
+        const[estMembers, setEstmembers]=useState([])
+        const [number,setNumber]=useState(0)
+      // const [newEstMembersDetails,setnewEstMembersDetails]=useState({memberId:"",estTimeMember:""});
+       const [newEstMembersDetails,setnewEstMembersDetails]=useState([]);
+//FETCH SELECTED PROJECT DATA 
+useEffect(() => {
+    getProjects();
+}, [])
+
+const getProjects = () => {
+    get("/projects/1")
+        .then(data => { 
+            
+            setOneProject(data);
+           
+        })
+};
+console.log(oneProject);
 //FUNCTION THAT ONCHANG INPUT WILL RUN 
         const addEstimateTime=(event)=>{
-            {console.log('Input Id:',event.target.id)}
-            {console.log('Input value:',event.target.value)}
-            projects.filter(projectId=>projectId.id==1).map((goToIssueList)=>
-            
-            patch('/projects/'+ 1,{estTimeMember:event.target.value})
-            .then(goToIssueList=>(
-                <div>
-                    {console.log("goToIssueList",goToIssueList.issueList)}
-                    
-                    {(typeof(goToIssueList.issueList=='object'))? 
-                        <div>
-                           
-                            {goToIssueList.issueList.filter(goToIssueId=>goToIssueId.issueId==event.target.id ).map((goToIssueId) =>
-                                    <div>
-                                        {console.log(goToIssueId)}
-                                        { (typeof(goToIssueId.estMembers=='object'))?
-                                            <div>
-                                            {goToIssueId.estMembers.filter(goToMemberId=>goToMemberId.memberId==2).map(goToMemberId=>
-                                            <div key={uuid}>
-                                           
-                                               
-                                                    {/* { setProjects(goToMemberId=>{return[...goToMemberId,{estTimeMember:event.target.id}]})}  */}
-                                                    {console.log('EstTimeMember value in Array',goToMemberId.estTimeMember)}
-                                                    {console.log('EstTimeMember value from input',event.target.value)}                                          
-                                                </div>
-                                            
-                                                )}
-                                            </div>
-                                        : null}
-                                    </div>
-                                )}
-                        </div>
-                    : null}
-                </div>)
-                )
-                )
-        }
-       
-    return (
-        <div className={styles.estimateprojektContainer} >
-            {projects.filter(issue=>issue.id==1).map((issue, i) => (
-                <div key={uuid}>
-                    <h1>{issue.projectName}</h1>
-                        {
-                            (typeof(issue.issueList)=='object')?
-                                 <div className={styles.issuesBoxContainer}>
-                                    <div className={styles.issueList}>
-                                        
-                                        {issue.issueList.map((issueItem,k)=>
-                                            <div>
-                                                <div  className={styles.issueListNummber}>
-                                                    {issueItem.issueId} 
-                                                </div>
-                                               
-                                                <div  className={styles.issueListTitle}>
-                                                   
-                                                    {issueItem.title} 
-                                                </div>
-                                                <div key={uuid}>
-                                                <input  type="number"  id={issueItem.issueId}  onChange={addEstimateTime} />
-                                                </div>
-                                                
-                                            </div>
-                                            
-                                        )}
-                                    </div> 
-                                </div>
-                            :
-                            null
-                        }
-                              
-                
-                </div>
-
-            ))}
-            <div>
-        
-            </div>
-         </div>
-  
+            event.preventDefault();
+                    console.log(event.target.id);
+                    console.log(newEstMembersDetails);
+                    if(newEstMembersDetails.estTimeMember){
+                        setEstmembers(prevStateEstMember=>{return [...prevStateEstMember,{issueId:Number(event.target.id),estMembers:newEstMembersDetails.estTimeMember,memberId:newEstMembersDetails.memberId}]})
+                            
+                    }
+                  
+            }
            
+       
+        const devideIssue=()=>{
+            console.log(oneProject);
+            console.log(estMembers);
+             if(typeof(estMembers=='object')){
+                 estMembers.filter(issueId=>issueId.issueId==11).map((issueId,i)=>{
+                     let TempArray=[{issueId}];
+                     console.log('TemArray',TempArray);
+                     console.log(issueId);
+
+                     setOneProject(oneProject=>{
+
+                        if(typeof(oneProject.issueList=='object')){return oneProject.issueList.filter(goToIssueId=>goToIssueId.issueId==11 ). map((goToIssueId,k)=>{
+                           console.log(goToIssueId)
+                        goToIssueId.estMembers={issueId}
+                        }
+                        
+                    )}
+                    })
+                     })
+                  
+             }
+                 
+        }
+      
         
-         
+    return (
+       
+        <>
+      {/* THIS PART MUST BE DELETED AFTER #6 ISSUE OF REPO  */}
+                <div>
+                <button onClick={()=>{setnewEstMembersDetails(previousState=>{return{...previousState,memberId:"Sara"}})}}>Sara</button>
+                </div>
+                <div>
+                <button onClick={()=>{setnewEstMembersDetails(previousState=>{return{...previousState,memberId:"Emma"}})}}>Emma</button>
+                </div>
+        <div className={styles.container}>      
+           <table className='table table-bordered'>
+               <thead className='thead-dark'>
+                   <tr>
+                      <th>Add your Estimated time for each Issue </th>
+                      
+                   </tr>
+               </thead>
+               <tbody>
+                
+                   {projects.filter(project=>project.id==1).map((project) => <div>
+                    {
+                (typeof(project.issueList)=='object')?
+                                        
+                                        
+                <div key={uuid}>
+                     {project.issueList.map((issueItem,k)=>
+                      <tr>
+                        <td> {issueItem.issueId}</td>
+                        <td>{issueItem.title}</td>
+                         <td><input  type="number"  id={issueItem.issueId}  onChange={(event)=>{setnewEstMembersDetails(previousState=>{return{...previousState,estTimeMember:event.target.value}})}} /> </td>
+                        <td> <button onClick={addEstimateTime} id={issueItem.issueId}>Add estimated time</button></td>
+                    </tr> )}
+               
+                </div> 
+                :null }
+                   </div>)}
+                 
+               </tbody>
+           </table>
+           <button onClick={devideIssue}>devide</button>
+        </div>
+        
+         </>
+       
     )  
 }
+
+
